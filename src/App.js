375a6e3fe526175729df69b9a04ed5d661b8189a
Yusuke Shimizu <karahiyo@users.noreply.github.com>
2020-03-09 22:45:30 +0900

    update App.js to list todos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 
 import API, { graphqlOperation } from '@aws-amplify/api';
 import PubSub from '@aws-amplify/pubsub';
 
 import { createTodo } from './graphql/mutations';
+import { list todos } from './graphql/queries';
 
 import awsconfig from './aws-exports';
 import './App.css';
@@ -11,17 +12,51 @@ import './App.css';
 API.configure(awsconfig);
 PubSub.configure(awsconfig);
 
+// Action Types
+const QUERY = 'QUERY';
+
+const initialState = {
+    todos: [],
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case QUERY:
+            return {...state, todos: action.todos};
+        default:
+            return state;
+    }
+};
+
 async function createNewTodo() {
     const todo = { name: "Use AWS AppSync", description: "Realtime and offline" };
     await API.graphql(graphqlOperation(createTodo, { input: todo }));
 }
 
 function App() {
-  return (
-    <div className="App">
-      <button onClick={createNewTodo}>Add Todo</button>
-    </div>
-  );
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        async function getData() {
+            const todoData = await API.graphql(graphqlOperation(listTodos));
+            dispatch({ type: QUERY, todos: todoData.data.listTodos.items });
+        }
+        getData();
+    }, []);
+
+    return (
+        <div>
+        <div className="App">
+            <button onClick={createNewTodo}>Add Todo</button>
+        </div>
+        <div>
+        {state.todos.length > 0 ?
+            state.todos.map((todoen => <p key={todo.id}>{todo.name} : {todo.description}</p>):
+            <p>Add some todos!</p>
+        }
+        </div>
+        </div>
+    );
 }
 
 export default App;
